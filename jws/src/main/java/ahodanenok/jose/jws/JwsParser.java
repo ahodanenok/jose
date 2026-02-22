37680c486a3c00d101d05b96ea3b6f4540385805
Andrei Hodanenok <xoda91@gmail.com>
2026-02-22 20:42:56 +0300

    Parse jws json flattened representation

--- jws/src/main/java/ahodanenok/jose/jws/JwsParser.java
+++ jws/src/main/java/ahodanenok/jose/jws/JwsParser.java
@@ -3,6 +3,7 @@ package ahodanenok.jose.jws;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import ahodanenok.jose.common.Base64Url;
 import ahodanenok.jose.common.JsonParser;
@@ -44,8 +45,7 @@ public final class JwsParser {
         // todo: handle json not valid?
         // todo: no duplicate header params
         String protectedHeaderEncoded = parts[0];
-        JwsHeader protectedHeader = new JwsHeader(jsonParser.parse(new String(
-            Base64Url.decode(protectedHeaderEncoded, false), StandardCharsets.UTF_8)));
+        JwsHeader protectedHeader = parseProtectedHeader(protectedHeaderEncoded);
         // todo: verify crit parameter
         String payloadEncoded = parts[1];
         byte[] signature = Base64Url.decode(parts[2], false);
@@ -59,13 +59,43 @@ public final class JwsParser {
     }
 
     private JwsInput parseJsonFlat(String str) {
+        Map<String, Object> obj;
+        try {
+            obj = jsonParser.parse(str);
+        } catch (Exception e) {
+            throw new JwsException("Failed to parse JWS", e);
+        }
 
+        String protectedHeaderEncoded = (String) obj.get("protected");
+        JwsHeader protectedHeader = parseProtectedHeader(protectedHeaderEncoded);
+        String payloadEncoded = (String) obj.get("payload");
+        byte[] signature = Base64Url.decode((String) obj.get("signature"));
 
-        return null;
+        Jws jws = new JwsOneSignature(
+            Base64Url.decode(payloadEncoded, false), protectedHeader, signature, str);
+        boolean valid = verifySignature(
+            payloadEncoded, protectedHeaderEncoded, protectedHeader, signature);
+
+        return new JwsInput(jws, valid);
     }
 
     private JwsInput parseJson(String str) {
         return null;
+    }
+
+    private JwsHeader parseProtectedHeader(String protectedHeaderEncoded) {
+        String json = new String(
+            Base64Url.decode(protectedHeaderEncoded, false),
+            StandardCharsets.UTF_8);
+
+        Map<String, Object> params;
+        try {
+            params = jsonParser.parse(json);
+        } catch (Exception e) {
+            throw new JwsException("Failed to parse protected header", e);
+        }
+
+        return new JwsHeader(params);
     }
 
     private boolean verifySignature(
--- jws/src/test/java/ahodanenok/jose/jws/JwsParserTest.java
+++ jws/src/test/java/ahodanenok/jose/jws/JwsParserTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class JwsParserTest {
 
     @Test
-    public void parseCompactValid() {
+    public void testParseCompactValid() {
         JwsParser parser = JwsParser.builder()
             .forSerialization(JwsSerialization.COMPACT)
             .allowAlgorithm(new HS256Algorithm(new SecretKeySpec(Base64Url.decode("AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow"), "HmacSHA256")))
@@ -22,7 +22,8 @@ public class JwsParserTest {
             .create();
 
         // https://datatracker.ietf.org/doc/html/rfc7515#appendix-A.1.1
-        JwsInput input = parser.parse("eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk");
+        String str = "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk";
+        JwsInput input = parser.parse(str);
         assertTrue(input.isValid());
         Jws jws = input.accept();
         assertEquals("JWT", jws.getProtectedHeader().get("typ"));
@@ -33,10 +34,11 @@ public class JwsParserTest {
         assertArrayEquals(
             TestUtils.bytes(116, 24, 223, 180, 151, 153, 224, 37, 79, 250, 96, 125, 216, 173, 187, 186, 22, 212, 37, 77, 105, 214, 191, 240, 91, 88, 5, 88, 83, 132, 141, 121),
             jws.getSignature());
+        assertEquals(str, jws.asString());
     }
 
     @Test
-    public void parseCompactNotValid() {
+    public void testParseCompactNotValid() {
         JwsParser parser = JwsParser.builder()
             .forSerialization(JwsSerialization.COMPACT)
             .allowAlgorithm(new HS256Algorithm(new SecretKeySpec(Base64Url.decode("AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow"), "HmacSHA256")))
@@ -45,7 +47,8 @@ public class JwsParserTest {
 
         // https://datatracker.ietf.org/doc/html/rfc7515#appendix-A.1.1
         // payload modified - starts with uppercase 'E' instead of 'e'
-        JwsInput input = parser.parse("eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.EyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk");
+        String str = "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.EyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk";
+        JwsInput input = parser.parse(str);
         assertFalse(input.isValid());
         Jws jws = input.accept();
         assertEquals("JWT", jws.getProtectedHeader().get("typ"));
@@ -56,5 +59,69 @@ public class JwsParserTest {
         assertArrayEquals(
             TestUtils.bytes(116, 24, 223, 180, 151, 153, 224, 37, 79, 250, 96, 125, 216, 173, 187, 186, 22, 212, 37, 77, 105, 214, 191, 240, 91, 88, 5, 88, 83, 132, 141, 121),
             jws.getSignature());
+        assertEquals(str, jws.asString());
+    }
+
+    @Test
+    public void testParseJsonFlattenedValid() {
+        JwsParser parser = JwsParser.builder()
+            .forSerialization(JwsSerialization.JSON_FLAT)
+            .allowAlgorithm(new HS256Algorithm(new SecretKeySpec(Base64Url.decode("AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow"), "HmacSHA256")))
+            .withJsonParser(new JacksonJson())
+            .create();
+
+        // https://datatracker.ietf.org/doc/html/rfc7515#appendix-A.1.1
+        String str =
+        """
+        {
+            "payload": "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ",
+            "protected": "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9",
+            "signature": "dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk"
+        }
+        """;
+        JwsInput input = parser.parse(str);
+        assertTrue(input.isValid());
+        Jws jws = input.accept();
+        assertEquals("JWT", jws.getProtectedHeader().get("typ"));
+        assertEquals("HS256", jws.getProtectedHeader().get("alg"));
+        assertArrayEquals(
+            TestUtils.bytes(123, 34, 105, 115, 115, 34, 58, 34, 106, 111, 101, 34, 44, 13, 10, 32, 34, 101, 120, 112, 34, 58, 49, 51, 48, 48, 56, 49, 57, 51, 56, 48, 44, 13, 10, 32, 34, 104, 116, 116, 112, 58, 47, 47, 101, 120, 97, 109, 112, 108, 101, 46, 99, 111, 109, 47, 105, 115, 95, 114, 111, 111, 116, 34, 58, 116, 114, 117, 101, 125),
+            jws.getPayload());
+        assertArrayEquals(
+            TestUtils.bytes(116, 24, 223, 180, 151, 153, 224, 37, 79, 250, 96, 125, 216, 173, 187, 186, 22, 212, 37, 77, 105, 214, 191, 240, 91, 88, 5, 88, 83, 132, 141, 121),
+            jws.getSignature());
+        assertEquals(str, jws.asString());
+    }
+
+    @Test
+    public void testParseJsonFlattenedNotValid() {
+        JwsParser parser = JwsParser.builder()
+            .forSerialization(JwsSerialization.JSON_FLAT)
+            .allowAlgorithm(new HS256Algorithm(new SecretKeySpec(Base64Url.decode("AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow"), "HmacSHA256")))
+            .withJsonParser(new JacksonJson())
+            .create();
+
+        // https://datatracker.ietf.org/doc/html/rfc7515#appendix-A.1.1
+        // payload modified - starts with uppercase 'E' instead of 'e'
+        String str =
+        """
+        {
+            "payload": "EyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ",
+            "protected": "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9",
+            "signature": "dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk"
+        }
+        """;
+        JwsInput input = parser.parse(str);
+        assertFalse(input.isValid());
+        Jws jws = input.accept();
+        assertEquals("JWT", jws.getProtectedHeader().get("typ"));
+        assertEquals("HS256", jws.getProtectedHeader().get("alg"));
+        assertArrayEquals(
+            TestUtils.bytes(19, 34, 105, 115, 115, 34, 58, 34, 106, 111, 101, 34, 44, 13, 10, 32, 34, 101, 120, 112, 34, 58, 49, 51, 48, 48, 56, 49, 57, 51, 56, 48, 44, 13, 10, 32, 34, 104, 116, 116, 112, 58, 47, 47, 101, 120, 97, 109, 112, 108, 101, 46, 99, 111, 109, 47, 105, 115, 95, 114, 111, 111, 116, 34, 58, 116, 114, 117, 101, 125),
+            jws.getPayload());
+        assertArrayEquals(
+            TestUtils.bytes(116, 24, 223, 180, 151, 153, 224, 37, 79, 250, 96, 125, 216, 173, 187, 186, 22, 212, 37, 77, 105, 214, 191, 240, 91, 88, 5, 88, 83, 132, 141, 121),
+            jws.getSignature());
+        assertEquals(str, jws.asString());
     }
 }
